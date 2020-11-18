d3db3e9b76ba7665022e103a9968ed98ee89f140
Lucas <relave.lucas@gmail.com>
2020-11-18 18:33:00 +0100

    correctif

--- Views/index.php
+++ Views/index.php
@@ -6,24 +6,29 @@
 <body>
     <div class="row justify-content-md-center mx-auto">
             <div class="card col col-lg-2"  id="login" style=" margin-top: 200px;padding-top: 20px;padding-bottom: 50px; text-align:center;">
-                <form method="POST" action="index.php" >
+                <div class="col">
+                    <form method="POST" action="index.php" >
 
-                        <div class="form-group">
-                            <img src="../images/logo.png" id="logo" style="width: 150px;">
-                        </div>
+                            <div class="form-group">
+                                <img src="../images/logo.png" id="logo" style="width: 150px;">
+                            </div>
 
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email</label>
-                            <input type="text" name="login" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Entrer email" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Mot de passe</label>
-                            <input type="password" name="password" class="form-control" id="Password" placeholder="Mot de passe" required>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email</label>
+                                <input type="text" name="login" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Entrer email" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Mot de passe</label>
+                                <input type="password" name="password" class="form-control" id="Password" placeholder="Mot de passe" required>
 
 
-                        </div>
-                        <button type="submit" class="btn btn-primary">se connecter</button>
-                </form>
+                            </div>
+                            <button type="submit" class="btn btn-primary">se connecter</button>
+                    </form>
+                </div>
+                <div class="col mt-5">
+                    <button class="btn btn-primary">s'inscrire</button>
+                </div>
             </div>
         </div>
     </div>
