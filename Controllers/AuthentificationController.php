f54bd02923de5fb1126c22ce14700086c005be6e
Lucas <relave.lucas@gmail.com>
2020-11-18 18:28:51 +0100

    Authentification

---
+++ Controllers/AuthentificationController.php
@@ -0,0 +1,18 @@
+<?php
+    include('../Views/header.php');
+
+    if(isset($_POST['login']) and isset($_POST['password'])){
+        if(isset($_POST['login'])) {
+                    $login = htmlspecialchars($_POST['login']);
+            }
+        if(isset($_POST['password'])) {
+                    $password = htmlspecialchars($_POST['password']);
+            }
+
+            if($login  == "Admin@gmail.com" && $password == "admin") {
+                header('Location: /header.php'); 
+            }
+    }
+
+
+?>
